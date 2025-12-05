4f66bb39a3fe5dd5c548a87ace2cb76cdecd86e8
theAverageHuman <daniellynesgomez@gmail.com>
2025-12-05 20:50:54 +0000

    Struggled with day 2

--- Day5/rangeFinder.py
+++ Day5/rangeFinder.py
@@ -1,23 +1,4 @@
-file = open("test.txt")
-
-def consolidate():
-    for i in range(1, len(freshRanges)):
-        #print(freshRanges[i-1][1], " : ", freshRanges[i][0])
-        if int(freshRanges[i-1][1]) > int(freshRanges[i][0]):
-            mergedRanges.append([freshRanges[i-1][0], freshRanges[i][1]])
-        
-            if blendedRanges.count(i) == 0:
-                blendedRanges.append(i)
-            if blendedRanges.count(i-1) == 0:
-                blendedRanges.append(i-1)
-
-    #print(blendedRanges)
-    for i in blendedRanges:
-        mergedRanges.pop(i)
-
-    mergedRanges = sorted(mergedRanges, key=lambda x: int(x[0]))
-
-   
+file = open("input.txt")
 
 freshRanges = []
 #IDs = []
@@ -31,30 +12,52 @@ for line in file:
     elif rangesDone == False:
         freshRanges.append(line.replace('\n', '').split("-"))
 
+#print(len(freshRanges))
+
 #print(IDs)
 
 freshRanges = sorted(freshRanges, key=lambda x: int(x[0]))
-mergedRanges = freshRanges.copy()
 
-blendedRanges = []
+def consolidate(mergedRanges):
+    global freshRanges
 
-for i in range(1, len(freshRanges)):
-    #print(freshRanges[i-1][1], " : ", freshRanges[i][0])
-    if int(freshRanges[i-1][1]) > int(freshRanges[i][0]):
-        mergedRanges.append([freshRanges[i-1][0], freshRanges[i][1]])
+    blendedRanges = []
+    hasMerged = False
+
+    for i in range(1, len(freshRanges)):
+        #print(freshRanges[i-1][1], " : ", freshRanges[i][0])
+        if int(freshRanges[i-1][1]) > int(freshRanges[i][0]):
+            mergedRanges.append([freshRanges[i-1][0], freshRanges[i][1]])
         
-        if blendedRanges.count(i) == 0:
-            blendedRanges.append(i)
-        if blendedRanges.count(i-1) == 0:
-            blendedRanges.append(i-1)
+            if blendedRanges.count(i) == 0:
+                blendedRanges.append(i)
+            if blendedRanges.count(i-1) == 0:
+                blendedRanges.append(i-1)
 
-#print(blendedRanges)
-for i in blendedRanges:
-    mergedRanges.pop(i)
+            hasMerged = True
 
-mergedRanges = sorted(mergedRanges, key=lambda x: int(x[0]))
+    blendedRanges = sorted(blendedRanges, reverse=True)
+    #print(blendedRanges)
+    for i in blendedRanges:
+        #print(len(mergedRanges))
+        #print(i)
+        #print(mergedRanges[i])
+        mergedRanges.pop(i)
 
-print(freshRanges)
-print(mergedRanges)
+    mergedRanges = sorted(mergedRanges, key=lambda x: int(x[0]))
+    #print(mergedRanges)
+    freshRanges = mergedRanges.copy()
+    return hasMerged
+ 
+#print(freshRanges)
+
+while consolidate(freshRanges.copy()) == True:
+    magicNum = 0
+
+total = 0
+for rangee in freshRanges:
+    total += len(range(int(rangee[0]), int(rangee[1])+1))
+print(total)
+
 file.close()
 
